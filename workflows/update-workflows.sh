d4ccb14216f4ae21ad71c77862c7339e466d8f2b
Maxat Kulmanov <coolmaksat@gmail.com>
2023-12-10 10:28:06 +0300

    update workflows

--- workflows/update-workflows.sh
+++ workflows/update-workflows.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-arvados-cwl-runner --project-uuid=cborg-j7d0g-3yx09joxonkhbru --update-workflow=cborg-7fd4e-7zy0h7uhizql6vb pangenome-generate/pangenome-generate.cwl
-arvados-cwl-runner --project-uuid=cborg-j7d0g-3yx09joxonkhbru --update-workflow=cborg-7fd4e-zzk6vpo8d1k9zea fastq2fasta/fastq2fasta.cwl
+arvados-cwl-runner --project-uuid=cborg-j7d0g-8ekhzjdr669q81j --update-workflow=cborg-7fd4e-lfmtz87e7rhkqgm --name=ksa001-wes variant-calling/main.cwl variant-calling/ksa001-arvados.json
+arvados-cwl-runner --project-uuid=cborg-j7d0g-8ekhzjdr669q81j --update-workflow=cborg-7fd4e-62dvvpjfgfpfqne --name=ksa001-wgs variant-calling/main.cwl variant-calling/ksa001-arvados-wgs.json
