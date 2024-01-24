92bfbb8f8619bddd0aa2f2f67e41e87fd2a2d745
Yedige Radolda <edige.rad@gmail.com>
2024-01-24 14:28:18 +0300

    sync request bug is fixed

--- hguploader/main.py
+++ hguploader/main.py
@@ -20,9 +20,14 @@ import logging
 import requests
 from .qc_fastq import qc_fastq
 
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 ARVADOS_API_HOST = os.environ.get('ARVADOS_API_HOST', 'cborg.cbrc.kaust.edu.sa')
 ARVADOS_API_TOKEN = os.environ.get('ARVADOS_API_TOKEN', '')
 UPLOADER_URL = os.environ.get('UPLOADER_URL', 'https://hgupload.cbrc.kaust.edu.sa')
+
 
 def upload_file(col, filename_local, filename_remote):
     lf = open(filename_local, 'rb')
@@ -32,6 +37,7 @@ def upload_file(col, filename_local, filename_remote):
             f.write(r)
             r = lf.read(65536)
     lf.close()
+
 
 def validate_fastq(fastq_file):
     try:
@@ -45,13 +51,14 @@ def validate_fastq(fastq_file):
 def validate_metadata(metadata_file):
     schema_resource = pkg_resources.resource_stream(__name__, "schema.yml")
     cache = {
-        "https://raw.githubusercontent.com/bio-ontology-research-group/hguploader/master/hguploader/schema.yml": schema_resource.read().decode("utf-8")}
+        "https://raw.githubusercontent.com/bio-ontology-research-group/hguploader/master/hguploader/schema.yml": schema_resource.read().decode(
+            "utf-8")}
     (document_loader,
      avsc_names,
      schema_metadata,
      metaschema_loader) = schema_salad.schema.load_schema(
-         "https://raw.githubusercontent.com/bio-ontology-research-group/hguploader/master/hguploader/schema.yml",
-         cache=cache)
+        "https://raw.githubusercontent.com/bio-ontology-research-group/hguploader/master/hguploader/schema.yml",
+        cache=cache)
 
     shex = pkg_resources.resource_stream(
         __name__, "shex.rdf").read().decode("utf-8")
@@ -77,6 +84,7 @@ def validate_metadata(metadata_file):
         logging.warn(e)
     return False
 
+
 @ck.command()
 @ck.option(
     '--uploader-project', '-up', required=True,
@@ -84,32 +92,32 @@ def validate_metadata(metadata_file):
 @ck.option('--sequence-read1', '-sr1', required=True, help='Patient\'s FASTQ File (*.fastq.gz) read 1')
 @ck.option('--sequence-read2', '-sr2', help='Patient\'s FASTQ File (*.fastq.gz) read 2')
 @ck.option('--bed-file', '-bf', help='Patient\'s BED file for exome uploads')
-@ck.option('--bed-file-grch37', '-bf37', help='Patient\'s Orignial BED file with assembly version GRCh37(hg19) for exome uploads')
-
+@ck.option('--bed-file-grch37', '-bf37',
+           help='Patient\'s Orignial BED file with assembly version GRCh37(hg19) for exome uploads')
 @ck.option('--father-sequence-read1', '-fsr1', help='Father\'s FASTQ File (*.fastq.gz) read 1')
 @ck.option('--father-sequence-read2', '-fsr2', help='Father\'s FASTQ File (*.fastq.gz) read 2')
 @ck.option('--father-bed-file', '-fbf', help='Father\'s BED file for exome uploads')
-@ck.option('--father-bed-file-grch37', '-fbf37', help='Father\'s orignial BED file with assembly version GRCh37(hg19) for exome uploads')
-
+@ck.option('--father-bed-file-grch37', '-fbf37',
+           help='Father\'s orignial BED file with assembly version GRCh37(hg19) for exome uploads')
 @ck.option('--mother-sequence-read1', '-msr1', help='Mother\'s FASTQ File (*.fastq.gz) read 1')
 @ck.option('--mother-sequence-read2', '-msr2', help='Mother\'s FASTQ File (*.fastq.gz) read 2')
 @ck.option('--mother-bed-file', '-mbf', help='Mother\'s BED file for exome uploads')
-@ck.option('--mother-bed-file-grch37', '-mbf37', help='Mother\'s Orignial BED file with assembly version GRCh37(hg19) for exome uploads')
-
+@ck.option('--mother-bed-file-grch37', '-mbf37',
+           help='Mother\'s Orignial BED file with assembly version GRCh37(hg19) for exome uploads')
 @ck.option('--sibling-sequence-read1', '-ssr1', help='Sibling\'s FASTQ File (*.fastq.gz) read 1')
 @ck.option('--sibling-sequence-read2', '-ssr2', help='Sibling\'s FASTQ File (*.fastq.gz) read 2')
 @ck.option('--sibling-bed-file', '-sbf', help='Sibling\'s BED file for exome uploads')
-@ck.option('--sibling-bed-file-grch37', '-sbf37', help='Sibling\'s Orignial BED file with assembly version GRCh37(hg19) for exome uploads')
-
+@ck.option('--sibling-bed-file-grch37', '-sbf37',
+           help='Sibling\'s Orignial BED file with assembly version GRCh37(hg19) for exome uploads')
 @ck.option('--pedigree-file', '-pf', help='Description of the familial relationships between samples.')
 @ck.option('--metadata-file', '-m', required=True, help='METADATA File')
 @ck.option('--no-sync', '-ns', is_flag=True)
 @ck.option('--upload-id', '-ui', help="Upload object id")
 def main(uploader_project, sequence_read1, sequence_read2, bed_file, bed_file_grch37,
-        father_sequence_read1, father_sequence_read2, father_bed_file, father_bed_file_grch37,
-        mother_sequence_read1, mother_sequence_read2, mother_bed_file, mother_bed_file_grch37,
-        sibling_sequence_read1, sibling_sequence_read2, sibling_bed_file, sibling_bed_file_grch37,
-        pedigree_file, metadata_file, no_sync, upload_id):
+         father_sequence_read1, father_sequence_read2, father_bed_file, father_bed_file_grch37,
+         mother_sequence_read1, mother_sequence_read2, mother_bed_file, mother_bed_file_grch37,
+         sibling_sequence_read1, sibling_sequence_read2, sibling_bed_file, sibling_bed_file_grch37,
+         pedigree_file, metadata_file, no_sync, upload_id):
     # if not validate_metadata(metadata_file):
     #     return
     metadata = yaml.load(open(metadata_file), Loader=yaml.FullLoader)
@@ -132,7 +140,6 @@ def main(uploader_project, sequence_read1, sequence_read2, bed_file, bed_file_gr
         upload_file(col, bed_file, 'variant_regions.bed')
         if bed_file_grch37:
             upload_file(col, bed_file_grch37, 'variant_regions_grch37.bed')
-
 
     is_father_exome = False
     is_father_paired = False
@@ -181,7 +188,7 @@ def main(uploader_project, sequence_read1, sequence_read2, bed_file, bed_file_gr
         upload_file(col, sibling_bed_file, 'sibling_variant_regions.bed')
         if sibling_bed_file_grch37:
             upload_file(col, sibling_bed_file_grch37, 'sibling_variant_regions_grch37.bed')
-    
+
     upload_file(col, pedigree_file, 'pedigree.ped')
     upload_file(col, metadata_file, 'metadata.yaml')
     sample_id = str(metadata['id']) + '_' + metadata['patient']['identifier']
@@ -221,8 +228,7 @@ def main(uploader_project, sequence_read1, sequence_read2, bed_file, bed_file_gr
             'status': 'uploaded'
         }
         # Synchronize the upload on the web
-        r = requests.post(UPLOADER_URL + '/api/uploader/sync', data=data)
-    
+        requests.post(UPLOADER_URL + '/api/sync', verify=False, data=data)
 
     # res_uri = ARVADOS_COL_BASE_URI + response['uuid']
     # graph = to_rdf(res_uri, args.metadata.name)
